dfb1c4a8176e57f7c91a2e9643639045c01e068c
laura perlaza <117466164+LauPerlaza@users.noreply.github.com>
2023-04-11 14:59:49 -0500

    added terraform aws networking (#2)
    * added terraform aws networking
    
    * add ec2 and fix errors
    
    * ec2 configuration fix

---
+++ infrastructure/variables.tf
@@ -0,0 +1,13 @@
+variable "region" {
+  default     = "us-east-1"
+  type        = string
+  description = "Region of the VPC"
+}
+variable "Environment" {
+   type = string
+   description = "dev"
+}
+variable "ip" {
+type = string
+description = "ip"
+}
